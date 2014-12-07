918a8e489e833a7c67f1071fd17043a896905e47
rokemba <ngromaricokemba@gmail.com>
2014-12-07 18:36:08 +0000

    ok

--- gestion-stock-entities/src/Test.java
+++ gestion-stock-entities/src/Test.java
@@ -1,9 +1,8 @@
-
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello");
+		System.out.println("Hello Romaric Okemba");
 	}
 
 }
